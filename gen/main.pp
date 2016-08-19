36eb30a99589f4b49f47450420d1984882f4e1ac
kiran4399 <kiran4399@gmail.com>
2016-08-19 07:15:01 +0000

    beta testing for pru firmware

---
+++ gen/main.pp
@@ -0,0 +1,1 @@
+gen/main.object: main.c
--- main.c
+++ main.c
@@ -1,40 +1,6 @@
 /* Modified by Kiran Kumar Lekkala <kiran4399@gmail.com>
-*/
 
-/*
- * Copyright (C) 2015 Texas Instruments Incorporated - http://www.ti.com/
- *
- *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions
- * are met:
- *
- *	* Redistributions of source code must retain the above copyright
- *	  notice, this list of conditions and the following disclaimer.
- *
- *	* Redistributions in binary form must reproduce the above copyright
- *	  notice, this list of conditions and the following disclaimer in the
- *	  documentation and/or other materials provided with the
- *	  distribution.
- *
- *	* Neither the name of Texas Instruments Incorporated nor the names of
- *	  its contributors may be used to endorse or promote products derived
- *	  from this software without specific prior written permission.
- *
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
- * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
- * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
- * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
- * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
- * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
- * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
- * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
- * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
- * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
- * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- */
 
-#include <stdint.h>
 #include "pru_cfg.h"
 #include "pru_ctrl.h"
 #include "pru_intc.h"
@@ -44,8 +10,8 @@ volatile register uint32_t __R30;
 volatile register uint32_t __R31;
 
 /* Mapping Constant table register to variable */
-volatile far uint32_t CT_L3 __attribute__((cregister("L3OCMC", near), peripheral));
-volatile far uint32_t CT_DDR __attribute__((cregister("DDR", near), peripheral));
+//volatile far uint32_t CT_L3 __attribute__((cregister("L3OCMC", near), peripheral));
+//volatile far uint32_t CT_DDR __attribute__((cregister("DDR", near), peripheral));
 
 /* PRU-to-ARM interrupt */
 #define PRU0_ARM_INTERRUPT (19+16)
@@ -69,21 +35,31 @@ volatile far uint32_t CT_DDR __attribute__((cregister("DDR", near), peripheral))
 
 void main(void)
 {
-	volatile uint32_t mask;
-	volatile uint32_t *pDdr = (uint32_t *) &CT_DDR;
+	volatile uint32_t mask=0;
+	//volatile uint32_t *pDdr = (uint32_t *) &CT_DDR;
 	double pulse_width[SERVO_NUM_PIN],pulse_original[SERVO_NUM_PIN], time_step = 1/10000;
 	double period = 1/50;
 	int pin;
 	
-	pulse_width[0] = pDdr[6];
+	/*pulse_width[0] = pDdr[6];
 	pulse_width[1] = pDdr[7];
 	pulse_width[2] = pDdr[4];
 	pulse_width[3] = pDdr[5];
 	pulse_width[4] = pDdr[0];
 	pulse_width[5] = pDdr[2];
 	pulse_width[6] = pDdr[1];
-	pulse_width[7] = pDdr[3];
+	pulse_width[7] = pDdr[3];*/
 
+	
+	pulse_width[0] = 50;
+	pulse_width[1] = 50;
+	pulse_width[2] = 50;
+	pulse_width[3] = 50;
+	pulse_width[4] = 50;
+	pulse_width[5] = 50;
+	pulse_width[6] = 50;
+	pulse_width[7] = 50;
+	
 	/* Clear SYSCFG[STANDBY_INIT] to enable OCP master port */
 	CT_CFG.SYSCFG_bit.STANDBY_INIT = 0;
 
@@ -97,20 +73,20 @@ void main(void)
 	PRU0_CTRL.CTPPR1 = 0x00003000;
 
 
-
-	while(1){
-
 		/* Pool for any receipt of interrupt on host 0 */
-		if ((__R31 & 0x40000000) != 0) {
+	//	while ((__R31 & 0x40000000) == 0) {
 			for(pin=0;pin<=SERVO_NUM_PIN-1;pin++){
 				pulse_width[pin] *= 0.01*period;
 				pulse_original[pin] = pulse_width[pin];
 			}
-		}
+	//	}
+
+	while(1){
+
 
 		for(pin=0;pin<=SERVO_NUM_PIN-1;pin++){
 				if(pulse_width[pin] > 0){
-					mask |= 1 << (pin+1);
+					mask |= 1 << (12-pin);
 					pulse_width[pin] -= time_step;
 				}
 
@@ -118,7 +94,7 @@ void main(void)
 					pulse_width[pin] = pulse_original[pin];
 				}
 				else if(pulse_width[pin] < 0){
-					mask &= ~(1<<(pin+1));
+					mask &= ~(1<<(12-pin));
 					pulse_width[pin] -= time_step;
 				}
 		}
